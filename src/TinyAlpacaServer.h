44f6f058c5749949a4335519c9290f2d48197ff2
James Synge <jamessynge@google.com>
2021-04-14 21:15:26 -0400

    Remove old version of ServerConnection; will later rename ServerConnection2 to ServerConnection.

--- src/TinyAlpacaServer.h
+++ src/TinyAlpacaServer.h
@@ -28,9 +28,7 @@
 #include "request_decoder.h"                 // IWYU pragma: export
 #include "request_decoder_listener.h"        // IWYU pragma: export
 #include "request_listener.h"                // IWYU pragma: export
-#include "server_connection.h"               // IWYU pragma: export
 #include "server_connection2.h"              // IWYU pragma: export
-#include "server_connections.h"              // IWYU pragma: export
 #include "server_description.h"              // IWYU pragma: export
 #include "server_socket_and_connection.h"    // IWYU pragma: export
 #include "server_sockets_and_connections.h"  // IWYU pragma: export
@@ -56,7 +54,6 @@
 #include "utils/platform_ethernet.h"         // IWYU pragma: export
 #include "utils/printable_cat.h"             // IWYU pragma: export
 #include "utils/printable_progmem_string.h"  // IWYU pragma: export
-#include "utils/server_connection_base.h"    // IWYU pragma: export
 #include "utils/server_socket.h"             // IWYU pragma: export
 #include "utils/socket_listener.h"           // IWYU pragma: export
 #include "utils/status.h"                    // IWYU pragma: export
