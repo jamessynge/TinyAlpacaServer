3e17119c07ed71d7b032056836dd79d4a84bbaea
James Synge <jamessynge@google.com>
2022-02-05 19:43:41 -0500

    Reduce includes needed by progmem_string_data.h to just the platform include.

--- src/literals.cpp
+++ src/literals.cpp
@@ -8,7 +8,7 @@ namespace alpaca {
 // ProgmemStringViews, which acts as a nested namespace.
 #define TAS_DEFINE_PROGMEM_LITERAL(name, literal)         \
   mcucore::ProgmemStringView ProgmemStringViews::name() { \
-    return PSV_128(literal);                              \
+    return MCU_PSV_128(literal);                          \
   }
 
 #include "literals.inc"
@@ -18,7 +18,7 @@ namespace alpaca {
 // Define static mcucore::ProgmemString factory methods in struct
 // ProgmemStrings, which acts as a nested namespace.
 #define TAS_DEFINE_PROGMEM_LITERAL(name, literal) \
-  mcucore::ProgmemString ProgmemStrings::name() { return PSD_128(literal); }
+  mcucore::ProgmemString ProgmemStrings::name() { return MCU_PSD_128(literal); }
 
 #include "literals.inc"
 
