cb59d30de43ddae0f4bb00b6ab4b7c86ae7aa7e9
James Synge <jamessynge@google.com>
2022-08-23 18:16:56 -0400

    Use test_tools in mcucore for testing RequestDecoder.
    (Basically, copied them earlier, now removing the originals.)

--- extras/tests/request_decoder_test.cpp
+++ extras/tests/request_decoder_test.cpp
@@ -25,6 +25,7 @@
 #include "glog/logging.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "mcucore/extras/test_tools/http1/string_utils.h"
 #include "request_decoder_listener.h"
 
 ABSL_DECLARE_FLAG(int, v);
@@ -35,6 +36,8 @@ namespace {
 
 constexpr const size_t kDecodeBufferSize = 40;
 
+using ::mcucore::test::AppendRemainder;
+using ::mcucore::test::GenerateMultipleRequestPartitions;
 using ::testing::EndsWith;
 using ::testing::HasSubstr;
 using ::testing::IsEmpty;
@@ -49,49 +52,10 @@ using ::testing::InSequence;
 using ::testing::Return;
 #endif  // TAS_ENABLE_REQUEST_DECODER_LISTENER
 
-std::vector<std::string> SplitEveryN(const std::string& full_request,
-                                     const size_t n) {
-  DVLOG(5) << "SplitEveryN " << n
-           << ", full_request.size: " << full_request.size();
-  CHECK_LE(n, mcucore::StringView::kMaxSize);
-  std::vector<std::string> partition;
-  for (size_t pos = 0; pos < full_request.size(); pos += n) {
-    DVLOG(6) << "pos: " << pos << ", pos+n: " << pos + n;
-    partition.push_back(full_request.substr(pos, n));
-    DVLOG(6) << "part: \"" << absl::CHexEscape(partition.back()) << "\"";
-  }
-  return partition;
-}
-
 std::vector<std::vector<std::string>> GenerateMultipleRequestPartitions(
     const std::string& full_request) {
-  DLOG(INFO) << "GenerateMultipleRequestPartitions; full_request (size="
-             << full_request.size() << "):\n"
-             << full_request;
-  std::vector<std::vector<std::string>> partitions;
-  size_t n = std::min(static_cast<size_t>(mcucore::StringView::kMaxSize),
-                      full_request.size());
-  bool first = true;
-  do {
-    auto partition = SplitEveryN(full_request, n);
-    partitions.push_back(partition);
-    if (first) {
-      // Start with an empty string.
-      partition.insert(partition.begin(), "");
-      first = false;
-    }
-  } while (--n > 0);
-  return partitions;
-}
-
-std::string AppendRemainder(const std::string& buffer,
-                            const std::vector<std::string>& partition,
-                            int ndx) {
-  std::string result = buffer;
-  for (; ndx < partition.size(); ++ndx) {
-    result += partition[ndx];
-  }
-  return result;
+  return GenerateMultipleRequestPartitions(full_request, kDecodeBufferSize,
+                                           kDecodeBufferSize);
 }
 
 bool TestHasFailed() {
